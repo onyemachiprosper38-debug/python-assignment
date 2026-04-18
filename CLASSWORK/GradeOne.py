9020487639f18463bacdec81e59d426113782501
onyemachiprosper38-debug <onyemachiprosper38@gmail.com>
2026-04-18 17:37:17 +0100

    python classwork

---
+++ CLASSWORK/GradeOne.py
@@ -0,0 +1,13 @@
+print('Enter grade')
+
+grade = 85
+
+if grade >= 60:
+
+print('passed'):
+
+elif grade <= 59:
+
+print('failed')
+
+
---
+++ CLASSWORK/purchaseammount.py
@@ -0,0 +1,26 @@
+ #the user will input the amount they have spent if the amount is less than 1,000 they got no discount else if the amount spent is greater than or equal to 1,000 to 10,000 they got 5% discount else if the ammount spent is greater than or equal to 10,000 t0 50,000 they 10% discount, if its above 50,000 they got 20% discount.
+
+
+print('enter price from 1,000 to 50,000')
+
+amount = 50000
+
+if amount >= 1000:
+
+print('you have gotten 5% discount'):
+
+elif amount < 1000:
+
+print('No discount for you')
+
+elif amount >= 10000:
+
+print('you have 10% discount')
+
+if amount >50000:
+
+print('congratulations you have gotten 20% discount')
+
+
+
+
